a5558c9b4698b7ccfb71412165cd85f0aa6965c7
Hartford Johnson <hartford89@gmail.com>
2019-12-15 17:38:46 -0500

    modified function

--- FunctionSpr.R
+++ FunctionSpr.R
@@ -4,15 +4,19 @@ SpringDat.R <- function(path) {
   datepat <- "\\d{2}\\/\\d{2}\\/\\d{2}"
   timepat <- "\\d{2}\\:\\d{2}\\:\\d{2} [AP]M"
   GMTpat <- "\\d{2}.\\d{2}"
-  Temppat <- "Temp\\.{3}[FC]"
+  Temppat <- "Temp[\\.Â]+?[FC]"
   for (i in 1:length(dataFiles)){
+    #separate 'Date.Time' into two separate columns
     DTCol <- dataFiles[[i]][, grepl("Date.Time", names(dataFiles[[i]]))]
     dataFiles[[i]]$Date <- str_extract(DTCol,datepat)
     dataFiles[[i]]$Time <- str_extract(DTCol,timepat)
     GMTval <- str_extract(names(dataFiles[[i]])[grepl("Date.Time", names(dataFiles[[i]]))], GMTpat)
+    #column name should look like "Time GMT-04:00"
     timecol <- paste0("Time, GMT-", substr(GMTval,1,2),":",substr(GMTval,4,5))
     names(dataFiles[[i]])[names(dataFiles[[i]])=="Time"] <- timecol
+    #drop original Date-Time column
     dataFiles[[i]] <- dataFiles[[i]][, !grepl("Date.Time", names(dataFiles[[i]]))]
+    #if Temp is in F, convert values and column name to C
     tempcolname <- names(dataFiles[[i]])[grepl("Temp", names(dataFiles[[i]]))]
     Fextrc <- str_extract(tempcolname, Temppat)
     is_F <- substr(Fextrc, nchar(Fextrc),nchar(Fextrc))=="F"
@@ -27,6 +31,7 @@ SpringDat.R <- function(path) {
     dataFiles[[i]] <- dataFiles[[i]] %>% 
       select(!!as.name(names(dataFiles[[i]])[1]), Date, !!as.name(timecol), if(is_F)newcolname else tempcolname, everything())
   }
+  #grouping identical locations
   filepattern <- "\\S+?_"
   locations <- c()
   for (i in 1:length(dataFiles)){
@@ -42,6 +47,7 @@ SpringDat.R <- function(path) {
     }
     output[[unique_location]] <- tmp
   }
+  #create a new folder, save files to folder
   dir.create("SpringData", showWarnings = F)
   for (i in 1:length(output)){
     filename <- paste0(substr(names(output)[i],1,nchar(names(output)[i])-1),".csv")
